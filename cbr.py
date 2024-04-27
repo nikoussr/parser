06ac1e22fcba19d3a82e16d6160ee54ef0f256c2
Захар <zog1@tpu.ru>
2024-04-28 00:00:48 +0700

    почистил методы + коменты + меню

--- cbr.py
+++ cbr.py
@@ -1,21 +1,17 @@
-import datetime
-
 import requests
 from bs4 import BeautifulSoup
-from selenium import webdriver
-import time
 import matplotlib.pyplot as plt
 
-from selenium.webdriver.common.by import By
 
-
-def req(url: str):  # all
+def req(url: str):
+    '''Парсит код страницы'''
     response = requests.get(url)
     data = response.text
     return data
 
 
-def find_currency(data):  # 1
+def find_currency(data):
+    """Находит курс кроны"""
     bs = BeautifulSoup(data, "html.parser")
     table = bs.find('table', class_='data')
     table_rows = table.find_all('tr')
@@ -29,46 +25,18 @@ def find_currency(data):  # 1
     return float(str(mas[4]).replace(',', '.')) / int(mas[2])
 
 
-def change_CZK_to_RUB(CZK, CZK_curs):  # 1
+def change_CZK_to_RUB(CZK, CZK_curs):
+    """Перевод крон в рубли"""
     return print(f"{CZK} чешских крон = {(CZK * CZK_curs).__round__(5)} рублей")
 
 
-def change_RUB_to_CZK(RUB, CZK_curs):  # 1
+def change_RUB_to_CZK(RUB, CZK_curs):
+    """Перевод рублей в кроны"""
     return print(f"{RUB} рублей = {(RUB / CZK_curs).__round__(5)} чешских крон")
 
 
-def currency_history(url):  # 2
-    date_1 = str(input("Введите первую дату: "))
-    date_2 = str(input("Введите вторую дату: "))
-    url = url[0] + date_1 + url[1] + date_2
-    return req(url)
-
-
-"""def find_currency_history(data):  # 2
-    date_1 = str(input("Введите первую дату: "))
-    date_2 = str(input("Введите вторую дату: "))
-    bs = BeautifulSoup(data, "html.parser")
-    choose_date_input_1= bs.find('input', class_ = 'datepicker-filter_input-from')['value'] = date_1
-    choose_date_input_2= bs.find('input', class_ = 'datepicker-filter_input-to')['value'] = date_2
-    print(choose_date_input_1, ' ', choose_date_input_2)
-    print(data)
-    table = bs.find('table', class_='data')
-    table_rows = table.find_all('tr')
-    mas = []
-    for tr in table_rows:
-        td = tr.find_all('td')
-        element = []
-        for i in td:
-            element.append(i.text.strip())
-        mas.append(element)
-    mas = mas[2:]
-    mas.reverse()
-    print(f"Дата:\t\tЕдиниц:\tЦена:")
-    for i in mas:
-        print(f"{i[0]}\t{i[1]}\t\t{i[2]}")"""
-
-
 def get_currency_data(start_date: str, end_date: str):
+    """"""
     url = f'https://www.cbr.ru/currency_base/dynamics/?UniDbQuery.Posted=True&UniDbQuery.mode=1&UniDbQuery.date_req1=&UniDbQuery.date_req2=&UniDbQuery.VAL_NM_RQ=R01760&UniDbQuery.From={start_date}&UniDbQuery.To={end_date}'
     response = requests.get(url)
     dates = []  # массив для дат
@@ -81,7 +49,7 @@ def get_currency_data(start_date: str, end_date: str):
         if table:
             rows = table.find_all('tr')
 
-            print(f"Дата\t\tКоличество\tЦена")
+            # print(f"Дата\t\tКоличество\tЦена")
             for row in rows:
                 cols = row.find_all('td')
                 if len(cols) >= 3:
@@ -90,7 +58,7 @@ def get_currency_data(start_date: str, end_date: str):
                     rate = float(cols[2].text.replace(',', '.'))
                     dates.append(date)
                     rates.append(rate)
-                    print(f'{date}\t {count}\t\t {rate}')
+                    # print(f'{date}\t {count}\t\t {rate}')
 
         """Строим таблицу"""
         plt.figure(figsize=(12, 6))
@@ -101,11 +69,13 @@ def get_currency_data(start_date: str, end_date: str):
         plt.xticks(rotation=45)
         plt.grid(True)
 
+        print('Сохранение данных, подождите...')
+
         """Сохранение таблицы"""
         plt.savefig(f'picture.png')
 
         """Сохранение файла"""
-        with open(f'rates', 'w') as file:
+        with open(f'rates.txt', 'w') as file:
             for date, rate in zip(dates, rates):
                 file.write(f'{date}\t{10}\t{rate}\n')
         plt.show()
@@ -115,15 +85,49 @@ def get_currency_data(start_date: str, end_date: str):
 
 def main():
     url_1 = 'https://cbr.ru/currency_base/daily/'
-    # CZK_curs = find_currency(data_curs_CZK)
+    while True:
+        print('Выберите действие:')
+        print('1. Перевод CZK в RUB')
+        print('2. Перевод RUB в CZK')
+        print('3. Получить историю курса валюты')
+        print('0. Выход\n')
+        choice = input('Введите номер действия: ')
+        print('=' * 80)
 
-    """if CZK_curs:
-        change_CZK_to_RUB(15, CZK_curs)
-        change_RUB_to_CZK(100, CZK_curs)"""
-    # Пример использования
-    start_date = input("Введите первую дату: ")
-    end_date = input("Введите вторую дату: ")
-    get_currency_data(start_date, end_date)
+        if choice == '1':
+            print("Для выхода - 0")
+            data_curs_CZK = req(url_1)
+            CZK_curs = find_currency(data_curs_CZK)
+            while True:
+                if CZK_curs:
+                    CZK_count = float(input("Введите количество крон: "))
+                    if CZK_count == 0:
+                        print('=' * 80)
+                        break
+                    change_CZK_to_RUB(CZK_count, CZK_curs)
+
+        elif choice == '2':
+            print("Для выхода - 0")
+            data_curs_CZK = req(url_1)
+            CZK_curs = find_currency(data_curs_CZK)
+            while True:
+                if CZK_curs:
+                    RUB_count = float(input("Введите количество рублей: "))
+                    if RUB_count == 0:
+                        print('=' * 80)
+                        break
+                    change_RUB_to_CZK(RUB_count, CZK_curs)
+
+        elif choice == '3':
+            start_date = input('Введите начальную дату (в формате dd_mm_yyyy): ')
+            end_date = input('Введите конечную дату (в формате dd_mm_yyyy): ')
+            get_currency_data(start_date, end_date)
+            print('=' * 80)
+        elif choice == '0':
+            print('Выход из программы.')
+            break
+        else:
+            print('Некорректный ввод. Пожалуйста, выберите 1 или 2.')
 
 
 if __name__ == '__main__':
